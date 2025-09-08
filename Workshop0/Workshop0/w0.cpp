7434944a418c9a9c0dc603ea9a09c8faab57d2d9
Caroline Chen <cchen247@myseneca.ca>
2025-09-08 13:46:04 -0400

    Completed workshop0

---
+++ Workshop0/Workshop0/w0.cpp
@@ -0,0 +1,22 @@
+//
+//  w0.cpp
+//  Workshop Zero
+//
+//  Created by Caroline Chen on 2025-09-08.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "#### Workshop Zero ####\n";
+    std::cout << "Subject and Section\n";
+    std::cout << "OOP244\n";
+    std::cout << "Name:\n";
+    std::cout << "Caroline Chen\n";
+    std::cout << "Student number\n";
+    std::cout << "112747241\n";
+    std::cout << "Email:\n";
+    std::cout << "cchen247@myseneca.ca\n";
+    return 0;
+}
